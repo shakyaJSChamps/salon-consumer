3397046bc47cf6ec405e4cf5bb54325b820616dc
aman <amishra73185@gmail.com>
2024-02-23 17:42:27 +0530

    salon list responsive

---
+++ app/salonlist/data.js
@@ -0,0 +1,74 @@
+export const SalonListData = [
+    {
+        title: "Big Boss",
+        location: "Karol Bagh",
+        rating: "4.0",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Connaught Place",
+        rating: "4.5",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Saket",
+        rating: "4.2",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Rajouri Garden",
+        rating: "4.7",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Vasant Kunj",
+        rating: "4.3",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Lajpat Nagar",
+        rating: "4.6",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Dwarka",
+        rating: "4.8",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Punjabi Bagh",
+        rating: "4.1",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Greater Kailash",
+        rating: "4.4",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    },
+    {
+        title: "Big Boss",
+        location: "Rohini",
+        rating: "4.9",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi culpa quaerat repellat nesciunt Lorem ipsum dolor"
+    }
+];
+
+
+export const ShopsCategory = [
+    "Pedicure", "Massage", "Hair cut", "Make-up", "Hairstyle"
+
+]
+
+export const Rating = [5, 4, 3, 2, 1]
+
+export const Distance = ["<1km", "1-5km", "5-10km", "10-15km", "anywhere"]
+
+export const Category = ["Male", "Female", "Unisex"]
