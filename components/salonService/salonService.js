c1f51ead38461eda18669bedc4f441afaf72769e
Anilv-jschamps <Anil@jschamps.com>
2024-03-05 10:12:25 +0530

    implementing salon detail page

--- components/salonService/salonService.js
+++ components/salonService/salonService.js
@@ -1,22 +1,30 @@
+"use client"
 import { Paper } from '@mui/material'
 import styles from './salonService.module.css'
+import { useState } from 'react';
 
 function SalonService() {
+  const [activeButton, setActiveButton] = useState('man');
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
   return (
     <div className={styles.container}>
-     <div className={styles.sevice}>
+      <div className={styles.sevice}>
         <Paper className={styles.paper}>
-            <h3>Select a Service</h3>
-            <div className={styles.serviceFor}>
-                <button>Male</button>
-                <button>Female</button>
-            </div>
-            <div className={styles.serviceType}>
-                
-            </div>
+          <h3>Select a Service</h3>
+          <div className={styles.serviceFor}>
+            <button onClick={() => handleButtonClick('man')} className={activeButton === 'man' ? styles.active : ''}>Male</button>
+            <button onClick={() => handleButtonClick('female')} className={activeButton === 'female' ? styles.active : ''}>Female</button>
+          </div>
+          <div className={styles.serviceType}>
+
+          </div>
         </Paper>
-     </div>
-     <div className={styles.seviceType}></div>
+      </div>
+      <div className={styles.seviceTypeDetail}>
+        <h3>BestSeller Haircut</h3>
+      </div>
     </div>
   )
 }
