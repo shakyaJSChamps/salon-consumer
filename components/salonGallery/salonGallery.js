4731cb546890e6bcd1b6a95240586bc351e562f5
anilv-jschamps <anil@jschamps.com>
2024-05-14 09:23:41 +0530

    fix error

--- components/salonGallery/salonGallery.js
+++ components/salonGallery/salonGallery.js
@@ -5,8 +5,8 @@ function SalonGallery({ data }) {
   console.log("Salongallery", data)
   return (
     <div className={styles.container}>
-        {data?.map(item => (
-            <img src={item} alt={'image'} 
+        {data?.map((item,index) => (
+            <img key={index} src={item} alt={'image'} 
             style={{width:"200px",height:"200px"}} />
         ))}
     </div>
